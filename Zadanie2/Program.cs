4dfd44924afa74bf858939f035850d2b8b2c6a46
sebnorth <sebnorth@gmail.com>
2021-01-08 15:46:04 +0100

    Dodaj pliki projektów.

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nazwa = @"C:\Users\sagata\Downloads\inne\kursy\C#\zaj5\pliki10\zaj08012021\kwadrat.txt";
+            FileStream plik = new FileStream(nazwa, FileMode.Append, FileAccess.Write, FileShare.None);
+            System.IO.StreamWriter zapis = new StreamWriter(plik);
+
+            for (int i = 0; i < 16; i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    zapis.Write("*");
+                }
+                zapis.WriteLine();
+            }
+
+            zapis.Close();
+            plik.Close();
+
+            // Console.ReadKey();
+        }
+    }
+}
---
+++ Zadanie3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Zadanie3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0; // licznik literki 'a' lub 'A'
+            int suma_w_linii = 0;
+            string nazwa = @"C:\Users\sagata\Downloads\inne\kursy\C#\zaj5\pliki10\zaj08012021\tekst.txt";
+            FileStream plik = new FileStream(nazwa, FileMode.Open, FileAccess.Read, FileShare.None);
+            System.IO.StreamReader odczyt = new StreamReader(plik);
+
+            string s;
+            while ((s = odczyt.ReadLine()) != null)
+            {
+                suma_w_linii = 0;
+                Console.WriteLine(s);
+                foreach (char znak in s)
+                {
+                    if (znak == 'a' || znak == 'A')
+                    {
+                        suma_w_linii++;
+                        suma++;
+                    }
+
+                }
+                Console.WriteLine("Suma literek 'a' lub 'A' w linii wynosi {0}", suma_w_linii);
+                Console.WriteLine();
+            }
+
+            odczyt.Close();
+            plik.Close();
+            Console.WriteLine("Suma literek 'a' lub 'A' w pliku wynosi {0}", suma);
+            Console.ReadKey();
+        }
+    }
+}
